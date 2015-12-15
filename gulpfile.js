f43f82e67366c6118a48225eac0f5caa604b9891
Robert Jesionek <yesionuk@gmail.com>
2015-12-15 22:17:34 +0100

    Further build improvements
    Added js uglify with source map generation - for all main js files.
    Leaving module js for time being.
    This will reduce the deployment server upload bandwidth.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var Server = require('karma').Server;
 var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 gulp.task('test', function (done) {
     new Server({
@@ -40,6 +42,15 @@ gulp.task('clean', function() {
     return del(['dist']);
 });
 
+gulp.task('js-uglify', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(sourcemaps.write('./', {addComment: false}))
+        .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'test', 'jshint');
+    gulp.start('styles', 'js-uglify', 'test', 'jshint');
 });
